8967a3f0d4aeec81b66f22a3ba50e343230e57ff
nexTab - Oliver Gehrmann <info@nextab.de>
2020-11-09 12:06:41 +0100

    functions.php snippet
    Add the content of this file inside the functions.php of your Child Theme.

---
+++ functions.php
@@ -0,0 +1,7 @@
+/* Load Child Theme Language Files */
+function nxt_translations() {
+    // load custom translation file for the parent theme
+    load_child_theme_textdomain( 'Divi', get_stylesheet_directory() . '/lang' );
+    load_child_theme_textdomain( 'et_builder', get_stylesheet_directory() . '/lang/builder' );
+}
+add_action( 'after_setup_theme', 'nxt_translations');
